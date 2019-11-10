b4d2ca4aabdc7a492202e9447d0a1d94ad343a19
majetis16 <majeti.s@husky.neu.edu>
2019-11-10 13:13:48 -0500

    First Commit

---
+++ src/main/java/com/example/myapp/serverjava/controllers/LocationController.java
@@ -0,0 +1,13 @@
+package com.example.myapp.serverjava.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins="*")
+public class LocationController {
+    
+    
+
+
+}

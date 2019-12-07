d7adfaf8836a44f2429e5781fd6d6c4e6cae0aaf
mkamesam <kamesam.m@husky.neu.edu>
2019-12-07 17:46:30 -0500

    started controllerz

--- src/main/java/com/example/myapp/controllers/UserController.java
+++ src/main/java/com/example/myapp/controllers/UserController.java
@@ -2,12 +2,17 @@ package com.example.myapp.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import com.example.myapp.models.User;
 import com.example.myapp.repositories.UserRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,4 +28,41 @@ public class UserController {
         return repository.findAllUsers();
     }
 
+    @GetMapping("/api/users/username/{username}")
+    public User findUserByUsername(@PathVariable("username") String username) {
+        return repository.findUserByUsername(username);
+    }
+
+    @GetMapping("/api/users/username/{username}/password/{password}")
+    public User findUserByCredentials(@PathVariable("username") String username, 
+                                      @PathVariable("password") String password) {
+        return repository.findUserByCredentials(username, password);
+    }
+
+    @GetMapping("/api/users/{userId}")
+    public User findUserById(@PathVariable("userId") Integer userId) {
+        Optional<User> optional = repository.findById(userId);
+        return optional.get();
+    }
+
+    @DeleteMapping("/api/users/{userId}")
+    public List<User> deleteUser(@PathVariable("userId") Integer userId) {
+        repository.deleteById(userId);
+        return this.findAllUsers();
+    }
+
+    @PutMapping("/api/users/{userId}")
+    public List<User> updateUser(@PathVariable("userId") Integer userId, @RequestBody User userUpdates) {
+        Optional<User> optional = repository.findById(userId);
+        User user = optional.get();
+        user.setPassword(userUpdates.getPassword());
+        repository.save(user);
+        return this.findAllUsers();
+    }
+
+    @PostMapping("/api/users")
+    public List<User> createUser(@RequestBody User user) {
+        repository.save(user);
+        return this.findAllUsers();
+    }
 }
--- src/main/java/com/example/myapp/repositories/UserRepository.java
+++ src/main/java/com/example/myapp/repositories/UserRepository.java
@@ -1,14 +1,33 @@
 package com.example.myapp.repositories;
+import com.example.myapp.models.User;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
-
-import com.example.myapp.models.User;
 
 
 public interface UserRepository extends CrudRepository<User, Integer> {
     @Query("SELECT user FROM User user")
     public List<User> findAllUsers();
+
+    @Query(value = "SELECT * FROM user WHERE username=:username", nativeQuery = true)
+    public User findUserByUsername(@Param("username") String uname);
+
+    @Query("SELECT u FROM User u WHERE u.username=:username AND u.password=:password")
+    public User findUserByCredentials(
+        @Param("username") String username,
+        @Param("password") String password);
+
+    @Query("SELECT u FROM User u WHERE u.id=:userId")
+    public User findUserById(@PathVariable("userId") Integer userId);
+
+    @Query("DELETE FROM User u WHERE u.id=:userId")
+    public List<User> deleteUser(@PathVariable("userId") Integer userId);
+
+    // @Query("UPDATE User u SET u.id=:userUpdates.id AND      WHERE u.id=:userId")
+    // public List<User> updateUser(@PathVariable("userId") Integer userId, 
+    //                              @RequestBody User userUpdates);
 }

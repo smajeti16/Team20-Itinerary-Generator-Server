727052f46445cff468664e55918b896c509ef1c7
mkamesam <kamesam.m@husky.neu.edu>
2019-12-09 17:55:09 -0500

    cleanup

--- src/main/java/com/example/myapp/controllers/ItineraryController.java
+++ src/main/java/com/example/myapp/controllers/ItineraryController.java
@@ -3,7 +3,6 @@ package com.example.myapp.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.example.myapp.models.Event;
 import com.example.myapp.models.Itinerary;
 import com.example.myapp.repositories.ItineraryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
@@ -46,10 +44,5 @@ public class ItineraryController {
     public Itinerary createItinerary(@RequestBody Itinerary itinerary) {
         return repository.save(itinerary);
     }
-
-    // @GetMapping("/api/itineraries/find/{itineraryId}")
-    // public List<Event> getEventsByItineraryId(@PathVariable("itineraryId") Integer itineraryId) {
-    //     return repository.getEventsByItineraryId(itineraryId); 
-    // }
 
 }
--- src/main/java/com/example/myapp/repositories/ItineraryRepository.java
+++ src/main/java/com/example/myapp/repositories/ItineraryRepository.java
@@ -2,25 +2,15 @@ package com.example.myapp.repositories;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import com.example.myapp.models.Event;
 import com.example.myapp.models.Itinerary;
 
 public interface ItineraryRepository extends CrudRepository<Itinerary, Integer> {
     @Query("SELECT itinerary FROM Itinerary itinerary")
     public List<Itinerary> findAllItineraries();
 
-    @Query("SELECT event FROM Event event ")
-    public List<Event> getEventsByItineraryId(@Param("itineraryId") Integer itineraryId);
-
-    // @Query("select u.userName from User u inner join u.area ar where ar.idArea = :idArea")
+    // @Query("SELECT event FROM Event event ")
+    // public List<Event> getEventsByItineraryId(@Param("itineraryId") Integer itineraryId);
 }
 
-// List<Object[]> authors = em.createQuery("SELECT a, b FROM Author a LEFT" + 
-//  "JOIN a.books b WHERE a.lastName = 'Janssen'").getResultList();
-
-
-
-// SELECT event, i FROM Event event JOIN e.id i WHERE i.itinerary_id=:itineraryId
